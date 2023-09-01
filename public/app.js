2d841e2f40ef43344587b5e6d00acf9942064e06
fyosetake <fyosetake@gmail.com>
2023-08-31 22:36:41 -0300

    Instala o 'Express.js'

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const appVue = new Vue({
+    el: '#app',
+    data: {
+      productName: '',
+      // Outros campos do produto
+    },
+    methods: {
+      registerProduct() {
+        // Lógica para enviar os dados do produto para o servidor
+      }
+    }
+  });  
