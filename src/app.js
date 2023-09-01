29ee5a3111997f24384b47a62ce4692bda607e79
fyosetake <fyosetake@gmail.com>
2023-08-31 21:10:41 -0300

    Adiciona lógica de 'stages' e operação de 'Block' e 'Blockchain'

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+const Blockchain = require('./Blockchain');
+const Block = require('./Block');
 const OrganicProduct = require('./OrganicProduct');
 
+const organoChain = new Blockchain();
 const product1 = new OrganicProduct(1, 'Maçã', '1234567890123', 'Frutas', 'Fazenda Santa Maria');
 
-console.log(product1);
+product1.addStage({ stage: 'Plantio', details: 'Espaldeira' });
+const block1 = new Block(1, new Date().toString(), product1.getName(), { stage: 'Plantio', details: 'Espaldeira' });
+organoChain.addBlock(block1);
+
+console.log('É um \"blockchain\" válido? ' + organoChain.isChainValid());
+console.log(JSON.stringify(organoChain, null, 2));
